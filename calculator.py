869e372de8b3e1636c9bf1ea7aca035f395451e4
goldenmilk123 <dhguswn10041004@gmail.com>
2026-05-19 15:19:59 +0900

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,7 @@
-
+num1 = float(input("enter the number:" ))
+operator = input("enter the operator(+,-): ")
+num2 = float(input("enter the number:" ))
+if operator = "+":
+  print(num1 + num2)
+elif operator = "-":
+  print(num1 - num2)
